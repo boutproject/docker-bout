edb649380d25ca00d4af78f405fe6146ca3525ac
Jarrod Leddy <jleddy@eris.txcorp.com>
2018-07-24 11:15:44 -0600

    update the start script for BOUT container to ask for cpus

--- start-BOUT.sh
+++ start-BOUT.sh
@@ -9,11 +9,23 @@ echo ""
 
 sudo docker images
 echo ""
-echo "Enter full path of shared storage directory:"
+echo "Enter full local path of shared storage directory:"
 read SHARED_DIR
 
 export IMAGE_NAME="bout-img"
 runcmd "mkdir $SHARED_DIR"
 
-runcmd 'sudo docker run --rm -it --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $SHARED_DIR:/home/bout-img-shared $IMAGE_NAME /bin/bash' #
+echo ""
+echo "How many cores should container utilize:"
+read NCORES
+
+MYCMD="sudo docker run --rm -it --tty --cpuset-cpus=\"0-$(($NCORES-1))\" -v $SHARED_DIR:/home/bout-img-shared $IMAGE_NAME /bin/bash"
+
+echo ""
+echo "Running command:"
+echo $MYCMD
+eval $MYCMD
 export containerId=$(sudo docker ps -l -q)
+
+## --env="DISPLAY" --env="QT_X11_NO_MITSHM=1"
+## --cpuset-cpus="0-$(($NCORES-1))"
